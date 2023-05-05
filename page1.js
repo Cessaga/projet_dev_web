1be03e99a98deb568f48969b559095ada6707b8f
Cessaga <124678726+Cessaga@users.noreply.github.com>
2023-05-05 14:01:48 +0200

    Delete page1.js

--- page1.js
+++
@@ -1,7 +0,0 @@
-var images = document.querySelectorAll('.images img');
-for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener('click', function() {
-    var url = this.parentNode.href;
-    window.location.href = url;
-  });
-}
