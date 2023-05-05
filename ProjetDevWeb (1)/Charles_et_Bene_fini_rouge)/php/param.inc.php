ca7d776ccc36131f535c8508de3f4d94c10fbb16
Cessaga <124678726+Cessaga@users.noreply.github.com>
2023-05-05 14:00:31 +0200

    Delete ProjetDevWeb (1) directory

--- ProjetDevWeb (1)/Charles_et_Bene_fini_rouge)/php/param.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("HOTE","webetu.univ-st-etienne.fr");
-define("UTILISATEUR","ec05265w");
-define("PASSE","4HM2I5P1");
-?>
--- ProjetDevWeb (1)/TD8/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connex.inc.php');
-include('create.php');
-$pdo = connexion('ev01484w');
-create($pdo);
-
-?>
